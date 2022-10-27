f3887a06ab985e029b95254b3b58a863d85b91ee
ravindrapv <ravindra.ravi4849@gmail.com>
2022-10-28 02:40:46 +0530

    handel setState-Altcamus-FAQS

--- block-BRaabi/news-app/src/components/News.jsx
+++ block-BRaabi/news-app/src/components/News.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import faqs from '../accordian/data.json'
 
 export default class News extends Component {
-  constructor(props){
-    super()
+  constructor(){
+    super() 
+    this.state = {
+      activeIndex:null,
+    }
   }
   render() {
     return (
@@ -24,19 +28,22 @@ export default class News extends Component {
             </div>
           </ui>
           <hr />
-          <div className="btn border-x-lime-300 center">open state</div>
+          <div className="btn border-x-lime-300 center">AltCampus FAQS</div>
           <ui className="center">
-            <div className="flex p-4 m-3 border justify-between">
-              <li>javascript</li>
-              <li>click</li>
-            </div>
-            <div className="flex p-4 m-3 border justify-between">
-              <li>node</li>
-              <li>click</li>
-            </div>
-            <div className="flex p-4 m-3 border justify-between">
-              <li>React</li>
-              <li>click</li>
+            <div>
+              {
+                faqs.map((faq,index) =>(
+                  <>
+                  <li>
+                  <h2 className=" bg-slate-100" onClick={() => {
+                    this.setState({activeIndex:index})
+                  }}><hr />
+                    {faq.Q}</h2>
+                  {index === this.state.activeIndex && <p>{faq.A}</p>}
+                  </li>
+                  </>
+                ) )
+              }
             </div>
           </ui>
         </div>
